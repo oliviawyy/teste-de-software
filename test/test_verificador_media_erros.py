07828e1ff6c6cfceb8380bfe865e51018c23dee2
oliviawyy <olivia.c@aluno.senai.br>
2026-03-06 16:08:23 -0300

    opa

--- test/test_verificador_media_erros.py
+++ test/test_verificador_media_erros.py
@@ -6,5 +6,12 @@ def test_string_como_entrada():
     with pytest.raises(TypeError, match="Tipo inválido, a entrada deve ser numérica"):
         verificador_media("OITO")
 
+def test_numero_negativo():
+    with pytest.raises(ValueError, match="Valor inválido, a média não pode ser negativa"):
+        verificador_media(-5)
+
+def test_media_acima_de_10():
+    with pytest.raises(ValueError, match="Valor inválido, a média deve ser entre 0 e 10"):
+        verificador_media(11)
 
 
